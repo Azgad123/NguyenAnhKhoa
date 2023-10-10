15fbd2db9a14ff2d9f46289fe124ed9fc2642934
Azgad <112394500+Azgad123@users.noreply.github.com>
2023-10-10 14:15:17 +0700

    first commit

---
+++ App.js
@@ -0,0 +1,16 @@
+// import ManHinh1 from "./manhinh/manHinh1";
+// import ManHinh2 from "./manhinh/manHinh2";
+// import ManHinh3 from "./manhinh/manHinh3";
+
+// import { View, Text } from "react-native";
+import Screen1 from "./week2/screen1";
+
+export default function App() {
+  return (
+    // <ManHinh1/>
+    // <ManHinh2/>
+    // <ManHinh3/>
+    // <Screen />
+    <Screen1 />
+  );
+}
